f655668a2509ad4a1d8eff95e9baa436770e73a5
Roman <kaspryshyn_roman@ukr.net>
2025-06-02 10:42:34 +0300

    add homework7

--- homework7/script.js
+++ homework7/script.js
@@ -35,6 +35,21 @@
 //   paragraph.style.fontSize = "20px";
 //   paragraph.style.fontFamily = "Comic Sans MS";
 // }
+// -------------------------------option 2----------
+
+// function changeCSS(){
+//     const paragraph = document.getElementById("text");
+//     paragraph.classList.toggle('changeParagraph');
+//     const button = document.querySelector('button');
+//     if(button.textContent === 'Change style!'){
+//         button.textContent = 'no style'
+//     }else{
+//         button.textContent ="Change style!"
+//     }
+    
+
+// }
+
 
 
 // 3. Задано сторінку з 3 кнопками і 1 лінкою. Напишіть Javascript код і реалізуйте HTML-сторінку з відповідними подіями на кожному елементові:
@@ -84,3 +99,80 @@
     //     alert("Немає обраного елемента для видалення!");
     //   }
     // }
+
+//     5. Реалізуйте програму, яка по натисканню на кнопку виводитиме повідомлення 
+// "I was pressed!", при наведенні на кнопку виводитиме повідомлення "Mouse on me!", а при відведенні курсора миші виводитиме повідомлення "Mouse is not on me!".
+// const button = document.querySelector('button');
+// const messageList = document.getElementById('messageList');
+
+// function addmessage (text){
+//     const li = document.createElement('li');
+//     li.textContent = text;
+//     messageList.appendChild(li)
+// }
+
+// button.addEventListener('click', ()=>addmessage('I was pressed'));
+// button.addEventListener('mouseover', ()=>addmessage('Mouse on me!'));
+// button.addEventListener('mouseout', ()=>addmessage('Mouse is not on me!'));
+
+
+// 6. Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) вікна браузера і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
+
+
+//   function updateWindowSize() {
+//             const width = window.innerWidth;
+//             const height = window.innerHeight;
+//             document.getElementById("windowSize").textContent = `Ширина: ${width}px, Висота: ${height}px`;
+//         }
+
+//         window.addEventListener("resize", updateWindowSize);
+//         updateWindowSize();
+
+
+
+// 7*. На сторінці потрібно реалізувати 2 випадаючих списки. У першому містяться назви країн, у другому – назви міст. Реалізувати роботу таким чином, щоб коли вибирається з лівого випадаючого списку певна країна - в правому випадаючому  списку з'являлися міста цієї країни. Список міст формується динамічно, через JavaScript. Також потрібно нижче вивести назву обраної країни і місто.
+
+
+    const citiesByCountry = {
+      ukraine: ["Київ", "Львів", "Харків"],
+      germany: ["Берлін", "Мюнхен", "Гамбург"],
+      usa: ["Нью-Йорк", "Лос-Анджелес", "Чикаго"]
+    };
+
+    const countrySelect = document.getElementById("country");
+    const citySelect = document.getElementById("city");
+    const resultDiv = document.getElementById("result");
+
+    countrySelect.addEventListener("change", function () {
+      const selectedCountry = this.value;
+      citySelect.innerHTML = "";
+
+      if (selectedCountry && citiesByCountry[selectedCountry]) {
+        citiesByCountry[selectedCountry].forEach(city => {
+          const option = document.createElement("option");
+          option.value = city;
+          option.textContent = city;
+          citySelect.appendChild(option);
+        });
+      } else {
+        const option = document.createElement("option");
+        option.textContent = "-- Спочатку оберіть країну --";
+        citySelect.appendChild(option);
+      }
+
+      // Очистити вивід
+      resultDiv.textContent = "";
+    });
+
+    citySelect.addEventListener("change", function () {
+      const countryText = countrySelect.options[countrySelect.selectedIndex].text;
+      const cityText = citySelect.value;
+      if (countryText && cityText) {
+        resultDiv.textContent = `Ви обрали: ${countryText}, місто: ${cityText}`;
+        
+        
+      }
+    });
+
+
+
